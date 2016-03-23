f17d2a27e517303a6ec980e0657f47639aa67593
cyz.aoj <cyz.aoj@gmail.com>
2016-03-23 17:32:08 +0800

    oauth2脚手架

--- oauth-server/src/main/java/com/tuicr/scaffold/server/config/CommonConfiguration.java
+++ oauth-server/src/main/java/com/tuicr/scaffold/server/config/CommonConfiguration.java
@@ -1,11 +1,6 @@
 package com.tuicr.scaffold.server.config;
 
-import com.google.code.kaptcha.impl.DefaultKaptcha;
-import com.google.code.kaptcha.util.Config;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.Properties;
 
 /**
  * @author ylxia
@@ -16,43 +11,6 @@ import java.util.Properties;
 @Configuration
 public class CommonConfiguration {
 
-
-    /*@Bean
-    public RestTemplate restTemplate() {
-        ClientHttpRequestFactory requestFactory = ClientHttpRequestFactorySelector.getRequestFactory();
-        RestTemplate restTemplate = new RestTemplate(requestFactory);
-        List<HttpMessageConverter<?>> converters = new ArrayList<>(3);
-        converters.add(new FormHttpMessageConverter());
-        converters.add(new FormMapHttpMessageConverter());
-        converters.add(new MappingJackson2HttpMessageConverter());
-        restTemplate.setMessageConverters(converters);
-        restTemplate.setErrorHandler(new LoggingErrorHandler());
-        return restTemplate;
-    }*/
-
-
-    /**
-     * 验证码生成器
-     *
-     * @return
-     */
-    @Bean
-    public DefaultKaptcha defaultKaptcha() {
-        DefaultKaptcha defaultKaptcha = new DefaultKaptcha();
-        Properties properties = new Properties();
-        properties.setProperty("kaptcha.border", "no");
-        properties.setProperty("kaptcha.border.color", "55,179,90");
-        properties.setProperty("kaptcha.textproducer.font.color", "blue");
-        properties.setProperty("kaptcha.image.width", "125");
-        properties.setProperty("kaptcha.image.height", "50");
-        properties.setProperty("kaptcha.border.color", "55,179,90");
-        properties.setProperty("kaptcha.textproducer.font.size", "40");
-        properties.setProperty("kaptcha.session.key", "code");
-        properties.setProperty("kaptcha.textproducer.char.length", "4");
-        Config config = new Config(properties);
-        defaultKaptcha.setConfig(config);
-        return defaultKaptcha;
-    }
 
 }
 
